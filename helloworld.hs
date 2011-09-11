33ef6a440156e4e1b868aab5ed6f612a21e8ca43
masaedw <masa.edw@gmail.com>
2011-09-12 01:45:48 +0900

    PageR

--- helloworld.hs
+++ helloworld.hs
@@ -5,11 +5,15 @@ data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
 / HomeR GET
+/page1 Page1R GET
+/page2 Page2R GET
 |]
 
 instance Yesod HelloWorld where
     approot _ = ""
 
-getHomeR = defaultLayout [whamlet|Hello World!|]
+getHomeR = defaultLayout [whamlet|<a href="@{Page1R}">Go to page 1!|]
+getPage1R = defaultLayout [whamlet|<a href="@{Page2R}">Go to page 2!|]
+getPage2R = defaultLayout [whamlet|<a href="@{HomeR}">Go home!|]
 
 main = warpDebug 3000 HelloWorld
