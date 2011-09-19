8c7bf54f58531e67738a7bfe7fe69b1623d44596
masaedw <masa.edw@gmail.com>
2011-09-19 10:28:05 +0900

    persistent

---
+++ Persistent.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
+import Database.Persist
+import Database.Persist.Sqlite (withSqliteConn, runSqlConn, runMigration)
+import Database.Persist.TH
+import Control.Monad.IO.Class (liftIO)
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
+Person
+    name String
+    age Int Maybe
+BlogPost
+    title String
+    authorId PersonId
+|]
+
+main :: IO ()
+main = withSqliteConn ":memory:" $ runSqlConn $ do
+    runMigration migrateAll
+
+    johnId <- insert $ Person "John Doe" $ Just 35
+    janeId <- insert $ Person "Jane Doe" Nothing
+
+    insert $ BlogPost "My fr1st p0st" johnId
+    insert $ BlogPost "One more for good measure" johnId
+
+    oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
+    liftIO $ print (oneJohnPost :: [(BlogPostId, BlogPost)])
+
+    john <- get johnId
+    liftIO $ print (john :: Maybe Person)
+
+    delete janeId
+    deleteWhere [BlogPostAuthorId ==. johnId]
